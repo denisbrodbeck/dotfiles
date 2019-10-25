c485590dfa70257e75ae52266ffa8f787527c674
Denis Brodbeck <go.github@darker.red>
2019-10-25 17:11:14 +0200

    Upgraded script for Fedora 31. Tasks done: * install php and composer * add basic ssh user config * update golang version link * add current vscode config * add user local npm support

--- init_fedora.sh
+++ init_fedora.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 set -eu
 
+echo 'Configuring Fedora 31 for DEV'
+
 sudo dnf -q -y update
-sudo dnf -q -y install ansible fish git gnome-tweak-tool nano nodejs open-vm-tools-desktop ShellCheck wireshark
-sudo dnf -q -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-29.noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-29.noarch.rpm
-sudo dnf -q -y config-manager --set-enabled fedora-cisco-openh264
-sudo dnf -q -y install gstreamer1-plugin-openh264 mozilla-openh264
+sudo dnf -q -y install fish nano gnome-tweak-tool open-vm-tools-desktop wireshark \
+                        git nodejs ansible ShellCheck \
+                        php-cli php-common php-gd php-pdo php-pgsql php-intl php-json php-xml php-sodium php-mbstring
+# sudo dnf -q -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-29.noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-29.noarch.rpm
+# sudo dnf -q -y config-manager --set-enabled fedora-cisco-openh264
+# sudo dnf -q -y install gstreamer1-plugin-openh264 mozilla-openh264
 
 # download nice fonts
 wget -q https://github.com/tonsky/FiraCode/raw/master/distr/otf/FiraCode-Regular.otf -O ~/Downloads/fonts_firacode_regular.otf
@@ -16,51 +20,127 @@ git config --global user.email "go.github@darker.red"
 git config --global user.name "Denis Brodbeck"
 git config --global core.autocrlf input
 
+# pre-init ssh
+mkdir -p -m 0700 ~/.ssh
+echo '# see https://infosec.mozilla.org/guidelines/openssh.html#modern
+# Ensure KnownHosts are unreadable if leaked - it is otherwise easier to know which hosts your keys have access to.
+HashKnownHosts yes
+# Host keys the client accepts - order here is honored by OpenSSH
+HostKeyAlgorithms ssh-ed25519-cert-v01@openssh.com,ssh-rsa-cert-v01@openssh.com,ssh-ed25519,ssh-rsa,ecdsa-sha2-nistp521-cert-v01@openssh.com,ecdsa-sha2-nistp384-cert-v01@openssh.com,ecdsa-sha2-nistp256-cert-v01@openssh.com,ecdsa-sha2-nistp521,ecdsa-sha2-nistp384,ecdsa-sha2-nistp256
+KexAlgorithms curve25519-sha256@libssh.org,ecdh-sha2-nistp521,ecdh-sha2-nistp384,ecdh-sha2-nistp256,diffie-hellman-group-exchange-sha256
+MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com
+Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
+
+Host *
+  ServerAliveInterval 60
+Host github.com
+  IdentityFile ~/.ssh/id_github
+' > ~/.ssh/config
+
+# install composer
+cd /tmp/
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'a5c698ffe4b8e849a443b120cd5ba38043260d5c4023dbf93e1558871f1f07f58274fc6f4c93bcfd858c6bd0775cd8d1') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+sudo php composer-setup.php --filename=composer --install-dir=/usr/local/bin
+php -r "unlink('composer-setup.php');"
+
 # install go
 mkdir -p ~/code/go/bin
-wget -q https://dl.google.com/go/go1.12.linux-amd64.tar.gz -O /tmp/setup_go.tgz
+wget -q https://dl.google.com/go/go1.13.3.linux-amd64.tar.gz -O /tmp/setup_go.tgz
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf /tmp/setup_go.tgz
-wget -q https://github.com/golang/dep/releases/download/v0.5.0/dep-linux-amd64 -O ~/code/go/bin/dep
-chmod 0755 ~/code/go/bin/dep
 
 # install vs code
 wget -q https://update.code.visualstudio.com/latest/linux-rpm-x64/stable -O /tmp/setup_code.rpm
 sudo dnf -q -y install /tmp/setup_code.rpm
 code --install-extension vscoss.vscode-ansible
-code --install-extension davidanson.vscode-markdownlint
 code --install-extension bungcip.better-toml
-code --install-extension peterjausovec.vscode-docker
+code --install-extension ms-vscode.csharp
 code --install-extension ms-vscode.go
-code --install-extension ryu1kn.partial-diff
-code --install-extension esbenp.prettier-vscode
+code --install-extension eamodio.gitlens
 code --install-extension editorconfig.editorconfig
+code --install-extension esbenp.prettier-vscode
+code --install-extension davidanson.vscode-markdownlint
 echo '{
-    "workbench.enableExperiments": false,
-    "workbench.startupEditor": "newUntitledFile",
-    "editor.fontFamily": "Fira Code",
+    "editor.fontSize": 16,
+    "editor.fontFamily": "FuraMono Nerd Font Mono",
     "editor.fontLigatures": true,
-    "window.zoomLevel": 1,
-    "files.eol": "\n",
-    "files.insertFinalNewline": true,
-    "files.trimTrailingWhitespace": true,
+    "editor.rulers": [
+        120
+    ],
+    "editor.smoothScrolling": true,
+    "editor.wordWrapColumn": 120,
     "explorer.confirmDelete": false,
     "explorer.confirmDragAndDrop": false,
     "explorer.openEditors.visible": 0,
-    "extensions.ignoreRecommendations": true,
-    "prettier.printWidth": 120
-}' > ~/.config/Code/User/settings.json
+    "extensions.showRecommendationsOnlyOnDemand": true,
+    "files.eol": "\n",
+    "files.insertFinalNewline": true,
+    "files.trimFinalNewlines": true,
+    "files.trimTrailingWhitespace": true,
+    "telemetry.enableTelemetry": false,
+    "telemetry.enableCrashReporter": false,
+    "window.zoomLevel": 0.2,
+    "workbench.enableExperiments": false,
+    "workbench.startupEditor": "newUntitledFile",
+    "emmet.includeLanguages": {
+        "vue-html": "html",
+        "javascript": "javascriptreact",
+        "nunjucks": "html"
+    },
+    "gitlens.defaultDateFormat": "DD.MM.YYYY HH:mm",
+    "gitlens.defaultDateShortFormat": "DD.MM.YYYY",
+    "php.validate.executablePath": "/usr/bin/php",
+    "prettier.printWidth": 120,
+    "prettier.trailingComma": "es5",
+    "go.useLanguageServer": true,
+    "[go]": {
+        "editor.snippetSuggestions": "none",
+        "editor.formatOnSave": true,
+        "editor.codeActionsOnSave": {
+            "source.organizeImports": true,
+        },
+        "editor.codeActionsOnSaveTimeout": 1500
+    },
+    "gopls": {
+        "usePlaceholders": true, // add parameter placeholders when completing a function
+        // ----- Experimental settings -----
+        "completeUnimported": true, // autocomplete unimported packages
+        "watchChangedFiles": true, // watch file changes outside of the editor
+        "deepComplete": true, // deep completion
+    },
+    "[javascript]": {
+        "editor.defaultFormatter": "esbenp.prettier-vscode"
+    },
+    "[json]": {
+        "editor.defaultFormatter": "esbenp.prettier-vscode"
+    },
+    "[html]": {
+        "editor.defaultFormatter": "esbenp.prettier-vscode"
+    },
+    "terminal.integrated.rendererType": "dom",
+    "[javascriptreact]": {
+        "editor.defaultFormatter": "esbenp.prettier-vscode"
+    }
+}
+' > ~/.config/Code/User/settings.json
 
 # init fish shell
 sudo usermod -s /usr/bin/fish denis
 wget -q https://get.oh-my.fish -O /tmp/setup_fish.fish
 fish /tmp/setup_fish.fish --noninteractive --yes
-echo '# setup go
+echo '#setup npm
+set -xg NPM_CONFIG_PREFIX $HOME/.npm-global
+set -xg PATH $HOME/.npm-global/bin $PATH
+# setup go
 set -xg PATH $PATH /usr/local/go/bin
 set -xg GOPATH $HOME/code/go
-set -xg PATH $PATH (go env GOPATH)/bin
+set -xg PATH (go env GOPATH)/bin $PATH
 # misc stuff
 set -xg DEFAULT_USER denis' > ~/.config/omf/init.fish
+
+# clean up
+rm -f /tmp/setup_*
 
 echo '
 Setup completed...
